3722982d1ef60c98df626a9f15dc84d3556a1ed1
Max-Malo <maxmalo.basley@gmail.com>
2026-05-04 10:10:30 +0200

    Update C2.3.sql

--- C2.3.sql
+++ C2.3.sql
@@ -4,6 +4,6 @@ select		hop.nom_hop
 from		hopital as hop
 			inner join service as serv on hop.idhop = serv.idhop
 
-group by	hop.idhop, hop.nom_hop
+group by	hop.nom_hop
 
 having		sum(serv.nb_lits) > 300;
