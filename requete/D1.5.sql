752be036e62f40a341f3f5c1c160a8d9beeb2664
Timothé Larbi <timothe.larbi@etu.univ-lehavre.fr>
2026-05-05 13:30:06 +0200

    modif d1.5.sql

--- requete/D1.5.sql
+++ requete/D1.5.sql
@@ -1,1 +1,10 @@
-/* 5. Écrire une requête qui donne la liste des consultations (nom du médecin, nom du patient) des patients de plus de 40 ans. Utiliser la fonction age_patient */
+/* 5. Écrire une requête qui donne la liste des consultations 
+(nom du médecin, nom du patient) des patients de plus de 40 ans. 
+Utiliser la fonction age_patient */
+
+select m.nom_med, 
+       p.nom_pat
+from   consulte c
+join   medecin m on c.idmed = m.idmed
+join   patient p on c.idpat = p.idpat
+where  age_patient(p.idpat) > 40;
