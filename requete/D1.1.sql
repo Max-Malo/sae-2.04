2bfbe531be31e7535a6b099ebdb8fb6a8b447b4f
Timothé Larbi <timothe.larbi@etu.univ-lehavre.fr>
2026-05-05 11:07:31 +0200

    Ajout D1.1.sql

---
+++ requete/D1.1.sql
@@ -0,0 +1,13 @@
+/* 1. Créer une vue qui regroupe les médecins chercheurs (nom, mail, spécialité) et le nom de leur hôpital. */
+
+create view medecin_chercheur as 
+
+select  m.nom_med  ,
+        m.mail_med ,
+        m.spec     ,
+
+        h.nom_hop  
+
+from    MEDECIN as m 
+        inner join 
+        HOPITAL as h on h.idHop = m.idHop ;
