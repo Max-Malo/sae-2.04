ca2812587577fb9ec52608a74289815857279789
Timothé Larbi <timothe.larbi@etu.univ-lehavre.fr>
2026-05-05 13:27:38 +0200

    modif D3 D4 .sql

--- requete/D1.3.sql
+++ requete/D1.3.sql
@@ -1,1 +1,10 @@
 /* 3. Créer une vue contenant les premières consultations des patients (idPat, premiere_date) */
+
+create view prem_consulte_patient as
+
+select c.idPat ,
+       c.MIN(date_consult) as premiere_date
+
+from   consulte as c 
+
+group by c.idPat;
--- requete/D1.4.sql
+++ requete/D1.4.sql
@@ -1,2 +1,22 @@
-/* 4. Utiliser cette vue pour écrire une fonction age_patient qui admet comme paramètre l’identifiant du patient et qui retourne l’âge du patient à sa première consultation */
+/* 4. Utiliser cette vue pour écrire une fonction age_patient qui admet comme paramètre l’identifiant du patient 
+et qui retourne l’âge du patient à sa première consultation */
 
+
+create or replace function age_patient( id_recherche int )
+returns int as $$
+
+declare 
+    v_age int ;
+begin
+
+    select  EXTRACT (YEAR FROM v.premiere_date) - EXTRACT(YEAR FROM p.date_nais)
+    into v_age
+
+    from    patient p
+    join    prem_consulte_patient v ON p.idPat = v.idPat
+
+    where   p.idPat = id_recherche;
+
+    RETURN v_age;
+end ;
+$$ LANGUAGE plpgsql;
