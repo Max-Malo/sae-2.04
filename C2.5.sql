1026c053725c56d936b4d4ffc02bf377fc13d8b5
TimotheLarbi <timothe.larbi@etu.univ-lehavre.fr>
2026-05-05 10:40:56 +0200

    Delete C2.5.sql

--- C2.5.sql
+++
@@ -1,7 +0,0 @@
-/*Le nombre de lits par type de service pour l'ensemble des hôpitaux*/
-select      serv.nom_serv, 
-			sum(serv.nb_lits) as total_lits
-
-from        service as serv
-
-group by    serv.nom_serv;
